6888e9f43c9c936f401fade903fd35321b60a47e
perezcleidy069-sketch <perezcleidy069@gmail.com>
2026-08-17 21:53:48 -0600

    feat(intermedio): Consultar

--- intermedio/ejercicio-028/resoluciones/cleidy-perez/dql/consultas.sql
+++ intermedio/ejercicio-028/resoluciones/cleidy-perez/dql/consultas.sql
@@ -1,9 +1,14 @@
 -- Consultas base. Completa o reemplaza segun el enunciado.
-USE campuslands_mysql;
-
-SELECT * FROM intermedio_ejercicio_028;
-
-SELECT categoria, COUNT(*) AS total_registros, AVG(puntaje) AS promedio
-FROM intermedio_ejercicio_028
-GROUP BY categoria
-ORDER BY promedio DESC;
+WITH MejoresPartidas AS (
+    SELECT 
+        j.gamertag,
+        m.nombre AS mision,
+        p.puntuacion_obtenida,
+        ROW_NUMBER() OVER(PARTITION BY p.id_jugador ORDER BY p.puntuacion_obtenida DESC) AS ranking
+    FROM partidas p
+    INNER JOIN jugadores j ON p.id_jugador = j.id
+    INNER JOIN misiones m ON p.id_mision = m.id
+)
+SELECT gamertag, mision, puntuacion_obtenida 
+FROM MejoresPartidas 
+WHERE ranking = 1;
